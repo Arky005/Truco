062242fa0d2adc582fedb30e554d06a24db04037
Otavio Nepomuceno Morais <otavio.morais@redemapa.agro>
2019-04-01 14:19:22 -0300

    Atualizacao cartas do jogador 2

--- src/truco/Jogo.java
+++ src/truco/Jogo.java
@@ -21,6 +21,8 @@ class Jogo extends javax.swing.JFrame {
     private Baralho baralho;
     private static Jogador jogador1, jogador2;//, jogador3, jogador4;
     private final int numJogadores;
+    private Thread threadJogo;
+    private Runnable runa;
     
     /**
      * Creates new form Jogo
@@ -47,61 +49,70 @@ class Jogo extends javax.swing.JFrame {
         
         
         
-        new Thread(){
-            @Override
-            public void run(){
-                
-                int jogadorSelecionado=(int)(Math.random()*2);
+       threadJogo = new Thread(new Runnable(){
+           boolean continuar=true;
+           public void run(){
+               
+              
+                   int jogadorSelecionado=(int)(Math.random()*2);
                     switch(jogadorSelecionado){
                         case 0:
                             Jogo.getJogador1().setTurno(true);
                         case 1:
                             Jogo.getJogador2().setTurno(true);
-                }
-                
-                while(true){
-                    
-        
-                    if(Jogo.getJogador2().isTurno()){
-                        jLabel1.setText("vez2");
-                        
-                        if(centroj1.getIcon()!=null)
-                        
-                        if(Jogo.getJogador2().getPontuacao()<10){
-                            
-                        } else {
-                            
-                        }
-                        
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException ex) {
-                            Logger.getLogger(Jogo.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                        Jogo.getJogador2().setTurno(false);
-                        Jogo.getJogador1().setTurno(true);
-                    } else 
-                    
-                    if(Jogo.getJogador1().isTurno()){
-                        jLabel1.setText("vez1");
-                        if(Jogo.getJogador1().getPontuacao()<10){
-                            
-                        } else {
-                            
-                        }
-                        if(centroj1.getIcon()!=null){
+                    }
+
+                    while(true){
+
+
+                        if(Jogo.getJogador2().isTurno()){
+                            jLabel1.setText("vez2");
+
+                            if(centroj1.getIcon()!=null)
+
+                            if(Jogo.getJogador2().getPontuacao()<10){
+
+                            } else {
+
+                            }
+
                             try {
                                 Thread.sleep(1000);
                             } catch (InterruptedException ex) {
                                 Logger.getLogger(Jogo.class.getName()).log(Level.SEVERE, null, ex);
                             }
-                            Jogo.getJogador1().setTurno(false);
-                            Jogo.getJogador2().setTurno(true);
+                            Jogo.getJogador2().setTurno(false);
+                            Jogo.getJogador1().setTurno(true);
+                        } else 
+
+                        if(Jogo.getJogador1().isTurno()){
+                            jLabel1.setText("vez1");
+                            if(Jogo.getJogador1().getPontuacao()<10){
+
+                            } else {
+
+                            }
+                            if(centroj1.getIcon()!=null){
+                                try {
+                                    Thread.sleep(1000);
+                                } catch (InterruptedException ex) {
+                                    Logger.getLogger(Jogo.class.getName()).log(Level.SEVERE, null, ex);
+                                }
+                                Jogo.getJogador1().setTurno(false);
+                                Jogo.getJogador2().setTurno(true);
+                            }
                         }
-                    }
-                }
-            }
-        }.start();
+                    
+                   
+               }
+               
+               
+           }
+           
+         
+       });
+        
+       threadJogo.start();
         
     }
     
@@ -119,10 +130,13 @@ class Jogo extends javax.swing.JFrame {
         j1carta2.clonarDe(Jogo.getJogador1().getMao().getCartas()[1]);
         j1carta3.clonarDe(Jogo.getJogador1().getMao().getCartas()[2]);
        
+        j2carta1.clonarDe(Jogo.getJogador2().getMao().getCartas()[0]);
+        j2carta2.clonarDe(Jogo.getJogador2().getMao().getCartas()[1]);
+        j2carta3.clonarDe(Jogo.getJogador2().getMao().getCartas()[2]);
         
-        j2c1.setIcon(new ImageIcon(getClass().getResource(("/imgs/fechada.png"))));
-        j2c2.setIcon(new ImageIcon(getClass().getResource(("/imgs/fechada.png"))));
-        j2c3.setIcon(new ImageIcon(getClass().getResource(("/imgs/fechada.png"))));
+       // j2c1.setIcon(new ImageIcon(getClass().getResource(("/imgs/fechada.png"))));
+       // j2c2.setIcon(new ImageIcon(getClass().getResource(("/imgs/fechada.png"))));
+       // j2c3.setIcon(new ImageIcon(getClass().getResource(("/imgs/fechada.png"))));
         iconeBaralho.setIcon(new ImageIcon(getClass().getResource(("/imgs/fechada.png"))));
         
         
@@ -159,14 +173,20 @@ class Jogo extends javax.swing.JFrame {
 
         centroj1 = new javax.swing.JLabel();
         iconeBaralho = new javax.swing.JLabel();
-        j2c1 = new javax.swing.JLabel();
-        j2c2 = new javax.swing.JLabel();
-        j2c3 = new javax.swing.JLabel();
         centroj2 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         j1carta2 = new truco.Carta();
         j1carta1 = new truco.Carta();
         j1carta3 = new truco.Carta();
+        j2carta1 = new truco.Carta();
+        j2carta2 = new truco.Carta();
+        j2carta3 = new truco.Carta();
+
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosed(java.awt.event.WindowEvent evt) {
+                formWindowClosed(evt);
+            }
+        });
 
         iconeBaralho.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -176,44 +196,39 @@ class Jogo extends javax.swing.JFrame {
 
         jLabel1.setText("jLabel1");
 
-        j1carta2.setText("carta2");
-
-        j1carta3.setText("carta3");
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(iconeBaralho, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(191, 191, 191))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(114, 114, 114)
-                        .addComponent(jLabel1)
-                        .addGap(130, 130, 130)))
-                .addComponent(j2c1, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(j2c2, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(j2c3, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(298, 298, 298))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(iconeBaralho, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(736, 736, 736))
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addContainerGap(476, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(centroj1, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(centroj2, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(277, 277, 277)
-                        .addComponent(j1carta1, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(j1carta2, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(j1carta3, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(58, 58, 58)
+                        .addComponent(jLabel1)
+                        .addGap(185, 185, 185)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(j2carta1, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(j2carta2, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(j2carta3, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(j1carta1, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(j1carta2, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(j1carta3, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                 .addGap(303, 303, 303))
         );
         layout.setVerticalGroup(
@@ -221,17 +236,17 @@ class Jogo extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(j2c3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(j2c2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(j2c1, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(24, 24, 24)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(j2carta1, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(j2carta2, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(j2carta3, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(72, 72, 72)
+                        .addGap(48, 48, 48)
                         .addComponent(jLabel1)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 22, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 193, Short.MAX_VALUE)
                 .addComponent(centroj2, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 24, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
@@ -240,7 +255,7 @@ class Jogo extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addGroup(layout.createSequentialGroup()
-                                .addGap(0, 166, Short.MAX_VALUE)
+                                .addGap(0, 0, Short.MAX_VALUE)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                                     .addComponent(j1carta1, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addComponent(j1carta2, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -259,6 +274,11 @@ class Jogo extends javax.swing.JFrame {
         JOptionPane.showMessageDialog(this, "Tira a mão do baralho, LADRÃO!");
     }//GEN-LAST:event_iconeBaralhoMouseClicked
 
+    private void formWindowClosed(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_formWindowClosed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel centroj1;
     private javax.swing.JLabel centroj2;
@@ -266,9 +286,9 @@ class Jogo extends javax.swing.JFrame {
     private truco.Carta j1carta1;
     private truco.Carta j1carta2;
     private truco.Carta j1carta3;
-    private javax.swing.JLabel j2c1;
-    private javax.swing.JLabel j2c2;
-    private javax.swing.JLabel j2c3;
+    private truco.Carta j2carta1;
+    private truco.Carta j2carta2;
+    private truco.Carta j2carta3;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
