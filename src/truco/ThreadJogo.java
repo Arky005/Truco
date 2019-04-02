84769d44112b054390a8ac3c0060acc18d1bf0f4
Otávio Nepomuceno Morais - 11821915 <11821915@animaedu.intranet>
2019-04-01 21:42:24 -0300

    Criacao da classe ThreadJogo

---
+++ src/truco/ThreadJogo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package truco;
+
+/**
+ *
+ * @author 11821915
+ */
+public class ThreadJogo extends Thread {
+    
+    public ThreadJogo(){
+        
+    }
+    
+    @Override
+    public void run(){
+        
+    }
+    
+}
